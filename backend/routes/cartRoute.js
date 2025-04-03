c343ffaeac0be02a4c846fbe0286878213b002e9
Aman Singh <amansinghbca8171@gmail.com>
2025-04-03 11:03:39 +0530

    first commit

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { addTOCart, getUserCart, updateCart } from '../controllers/cartController.js'
+import authUser from '../middleware/auth.js'
+
+const cartRouter = express.Router()
+
+cartRouter.post('/get',authUser,getUserCart)
+cartRouter.post('/add',authUser,addTOCart)
+cartRouter.post('/update',authUser,updateCart)
+
+export default cartRouter
