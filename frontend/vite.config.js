ac48de75408f490bf4669802d0ea5c84044b03db
Aman Singh <amansinghbca8171@gmail.com>
2025-04-03 11:23:55 +0530

    second

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port: 5173}
+  build: {
+    rollupOptions: {
+      external: ['mongoose']
+    }
+  }
 })
+
