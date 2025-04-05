a9f4fd3b118d3a4341981856e0d3e26f44ec4283
Aman Singh <93634882+amansinghbca8171@users.noreply.github.com>
2025-04-05 12:15:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend',
   plugins: [react()],
-  server:{port: 5173}
-})
+  server:{port: 5173},
+  build: {
+    rollupOptions: {
+      external: ['mongoose'],
+      input: 'index.html'
+    }
+  }
+});
+
